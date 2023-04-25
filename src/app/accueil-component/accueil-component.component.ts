242799f823a2019e6ba422256a605beb94998aa5
l3miage-freundgm <matias.freund-galeano@etu.univ-grenoble-alpes.fr>
2023-04-25 13:47:36 +0200

    Accueil Front Done

---
+++ src/app/accueil-component/accueil-component.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil-component.component.html',
+  styleUrls: ['./accueil-component.component.scss']
+})
+export class AccueilComponent{
+  
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil-component/accueil-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    title: 'Accueil',
+    component: AccueilComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
